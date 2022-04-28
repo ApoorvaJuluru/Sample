5ec74bb9f75afb10729326fedcfd16343404df05
Apoorva <apoorvajuluru@gmail.com>
2022-04-28 11:31:13 +0530

    more files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionExmp
+{
+    internal class Program
+    {
+        static void ConsolePrint(int i)
+        {
+            Console.WriteLine(i);
+        }
+        static void Main(string[] args)
+        {
+            Action<int> printaction = new Action<int>(ConsolePrint);
+            printaction(10);
+        }
+    }
+}
